1b19f142a41c0327a11a65b376a696c559ace03c
root <root@cheezzyboii.host.mgmt.nest.internal>
2026-07-14 20:03:19 +0000

    Added some stuff

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.command("/bulldog-ping", async ({ command, ack, respond }) => {
   await ack();
   const latency = Date.now() - start;
   await respond({ text: `Pong!\nLatency: ${latency}ms\nWOOF! WOOF!` });
+  console.log("Someone used the ping command");
 });
 
 app.command("/bulldog-bark", async ({ command, ack, respond }) => {
@@ -22,6 +23,7 @@ app.command("/bulldog-bark", async ({ command, ack, respond }) => {
   await ack();
   const latency = Date.now() - start;
   await respond({ text: `WOOF! WOOF!\n ${latency}ms` });
+  console.log("Someone used the bark command");
 });
 
 app.command("/bulldog-fact", async ({ ack, respond }) => {
@@ -35,6 +37,7 @@ app.command("/bulldog-fact", async ({ ack, respond }) => {
 
     // Respond with the dog fact
     await respond({ text: `Dog Fact:\n${fact}` });
+    console.log("Someone used the dog fact command");
   } catch (err) {
     await respond({ text: "Failed to fetch a dog fact." });
   }
@@ -51,6 +54,7 @@ app.command("/bulldog-joke", async ({ ack, respond }) => {
 
 ${response.data.punchline}`
     });
+    console.log("Someone used the joke command");
   } catch (err) {
     await respond({ 
       response_type: "in_channel",
@@ -74,6 +78,7 @@ app.command("/bulldog-image", async ({ ack, respond }) => {
         }
       ]
     });
+    console.log("Some one used the dog image command");
   } catch (err) {
     await respond({ text: "WOOF ERROR WOOF! Possible time out please try again." });
   }
@@ -94,6 +99,7 @@ app.command("/bulldog-yes-no", async ({ ack, respond }) => {
         }
       ]
     });
+    console.log("Some one used the yes/no command");
   } catch (err) {
     await respond({ text: "Failed to fetch a yes/no answer." });
   }
@@ -114,6 +120,7 @@ Available Commands:
 /bulldog-image - Get a random dog image
 /bulldog-help - Show this help message`
   });
+  console.log("Someone used the help command");
 });
 
 
